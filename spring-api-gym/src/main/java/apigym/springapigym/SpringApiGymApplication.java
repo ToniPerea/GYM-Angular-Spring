370f70d1f8d9285f8c52d3be9292fc0a803bbe93
ToniPerea <apepa31@gmail.com>
2023-05-29 19:57:01 +0200

    first commit

---
+++ spring-api-gym/src/main/java/apigym/springapigym/SpringApiGymApplication.java
@@ -0,0 +1,13 @@
+package apigym.springapigym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiGymApplication.class, args);
+	}
+
+}
---
+++ spring-api-gym/src/test/java/apigym/springapigym/SpringApiGymApplicationTests.java
@@ -0,0 +1,13 @@
+package apigym.springapigym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
